fae9561b23c5d7741db0e6c1084f93891f6d2138
+ <+>
2022-12-09 16:39:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-function hasTargetSum(array, target) {
+//function hasTargetSum(array, target) {
+
   // Write your algorithm here
+
+
+function hasTargetSum(array, target) {
+  for(let i = 0; i<array.length; i++){
+    const complementary = target-array[i];
+    for(let j= i+1; j<array.length; j++){
+      if(array[j] ===complementary)return true;
+
+   }
+  }
+   return false;
 }
 
-/* 
+// Write your algorithm here
+/*
+//The function should return true if any pair of numbers in the array adds up to the target number.
+/*
   Write the Big O time complexity of your function here
 */
 
-/* 
+/*
   Add your pseudocode here
-*/
-
+*/ return true if any pair of numbers in the array adds up to the target number
+    else return false if no two numbers in the array adds up to the target
 /*
   Add written explanation of your solution here
-*/
+*/iterate over the array of numbers to check if any 2 numbers add upto the target
+for instance, if our number is 7 and target is 15, the complementary number is 8
+iterate over the remaining numbers in the array to check for any other complements
+return true if there are complements else return false
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
